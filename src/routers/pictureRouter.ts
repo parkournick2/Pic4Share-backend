d14351d4b6520703d829c2a65af836ed54dcd980
Nicolas Alexandre <61525227+parkournick2@users.noreply.github.com>
2021-07-20 13:41:53 -0300

    Update pictureRouter.ts

--- src/routers/pictureRouter.ts
+++ src/routers/pictureRouter.ts
@@ -4,6 +4,6 @@ import PictureController from "../controllers/PictureController";
 const pictureRouter = express.Router();
 
 pictureRouter.post("/create", PictureController.createPicture);
-pictureRouter.get("/search", PictureController.searchPicture);
+pictureRouter.post("/search", PictureController.searchPicture);
 
 export default pictureRouter;
